62c74844046005ef9603b7923084ed06dfa01c35
rodkawaura <rkmontemor@gmail.com>
2025-09-03 21:21:32 -0300

    added reports to ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
     baseUrl: 'http://localhost:8080/api/v3',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: true,
+      html: true
+    }
   },
   fixturesFolder: 'cypress/fixtures',
   videosFolder: 'cypress/videos',
